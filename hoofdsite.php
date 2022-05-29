ca7273551740950a82c438dd5dc9bd1f262c09cf
jelle <jelle.oeyen@icloud.com>
2022-05-29 18:05:48 +0200

    update
    nav bar gefixt

--- hoofdsite.php
+++ hoofdsite.php
@@ -5,12 +5,22 @@
         <nav>
                 <div class = "topnav">
                     <a class="active" href="hoofdsite.html">Home</a>
-                    <a href="bootstrap/">bootstrap</a>
                     <a href="eersteprobeersel.html">engineer</a>
                     <a href="javascript1.html">javascript</a>
                     <a href="hoofdsite.php">php</a>
+                    <a href="ajax.html">ajax</a>
+                    <div class="dropdown">
+                        <button class="dropbtn">bootstrap
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a href="bootstrap/container.html">container</a>
+                            <a href="bootstrap/fluidcontainer.html">fluidcontainer</a>
+                            <a href="bootstrap/gridbasic.html">gridbasic</a>
+                            </div>
+                    </div>
                 </div>
-        </nav>
+            </nav>
     </head>
     <body>
         <?php
@@ -19,5 +29,10 @@
         echo "vandaag is het" . date("Y/m/d") . "<br>";
         echo(pi());
         ?> 
+        <form action="welcome.php" method="post">
+        naam: <input type="text" name="naam"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+        </form>
     </body>
 </html>
